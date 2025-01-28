beefb531b9e40bfe4ab5f110b593adfb81ed4b19
Hans Tys <The.Hans.tys@gmail.com>
2025-01-28 22:46:45 +0100

    fixed typo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,7 +2,7 @@ import React, { useState ,useEffect } from "react";
 import './TodoList.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import  { faCircleCheck, faPen, faTrashCan , faArrowsRotate, faBan } from "@fortawesome/free-solid-svg-icons";
-import { spawn } from "child_process";
+
  
 
 
